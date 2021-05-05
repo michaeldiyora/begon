345cbac3b2043fdd3147f5da14d6331f89df19e5
michaeldiyora <michaeldiyora@gmail.com>
2021-05-05 17:19:54 +0530

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');
+const cssmin = require('gulp-clean-css');
+const jsmin = require('gulp-terser');
+const imgmin = require('gulp-imagemin');
+
+// ====================
+//html
+// ====================
+function copyhtml(cb) {
+  gulp.src('./src/index.html').pipe(gulp.dest('./public'));
+  cb();
+}
+exports.copyhtml = copyhtml;
+// ====================
+//sass
+// ====================
+function compilesass() {
+  return gulp
+    .src('./src/sass/*.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(prefix('last 5 version'))
+    .pipe(cssmin())
+    .pipe(gulp.dest('./public/css', { sourcemaps: '.' }));
+}
+exports.compilesass = compilesass;
+
+// ====================
+// js
+// ====================
+function minifyjs() {
+  return gulp
+    .src('./src/js/*.js', { sourcemaps: true })
+    .pipe(jsmin())
+    .pipe(gulp.dest('./public/js', { sourcemaps: '.' }));
+}
+exports.minifyjs = minifyjs;
+
+// ====================
+// image
+// ====================
+
+function optimizeimg() {
+  return gulp
+    .src('./src/images/*.{png,jpg}')
+    .pipe(
+      imgmin([
+        imgmin.mozjpeg({ quality: 75, progressive: true }),
+        imgmin.optipng({ optimizationLevel: 2 }),
+      ])
+    )
+    .pipe(gulp.dest('./public/images'));
+}
+exports.optimizeimg = optimizeimg;
+
+// ====================
+// run
+// ====================
+exports.run = gulp.series(copyhtml, compilesass, minifyjs, optimizeimg);
+// ====================
+// watch
+// ====================
+
+function watchTask() {
+  gulp.watch('./src/*.html', copyhtml);
+  gulp.watch('./src/sass/*.scss', compilesass);
+  gulp.watch('./src/js/*.js', minifyjs);
+  gulp.watch('./src/images/*.{png,jpg}', optimizeimg);
+}
+exports.watchTask = watchTask;
+
+// ====================
+// default
+// ====================
+
+exports.default = gulp.series(
+  copyhtml,
+  compilesass,
+  minifyjs,
+  optimizeimg,
+
+  watchTask
+);
---
+++ src/js/index.js
@@ -0,0 +1,125 @@
+'use strict';
+// ====================
+// navigation
+// ====================
+const scrollNav = document.querySelectorAll('.navigation__link,.nav__link');
+
+scrollNav.forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
+
+// --------------------
+// sticky navigation
+// --------------------
+
+window.addEventListener('scroll', function () {
+  const stickyNav = document.querySelector('.navigation');
+  stickyNav.classList.toggle('sticky', window.scrollY > 10);
+});
+
+// --------------------
+// onClick navigation hide
+// --------------------
+const hideNav = document.querySelectorAll('.navigation__link');
+
+hideNav.forEach(function (a) {
+  a.addEventListener('click', function () {
+    document.querySelector('.navigation__checkbox').checked = false;
+  });
+});
+
+// ====================
+// onscroll active link
+// ====================
+
+const sections = document.querySelectorAll(
+  '#home,#delivery,#service,#subscribe,#footer'
+);
+
+function navActive() {
+  let scrollDown = window.pageYOffset;
+
+  sections.forEach(function (present) {
+    const sectionHeight = present.offsetHeight;
+    const sectionUp = present.offsetTop - 175;
+    const sectionID = present.getAttribute('id');
+
+    if (scrollDown > sectionUp && scrollDown <= sectionUp + sectionHeight) {
+      document
+        .querySelector('.navigation__nav a[href*=' + sectionID + ']')
+        .classList.add('active');
+    } else {
+      document
+        .querySelector('.navigation__nav a[href*=' + sectionID + ']')
+        .classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', navActive);
+// ====================
+//tracking id
+// ====================
+const trackingBtn = document.querySelector('.btnb--tracking');
+const trackingID = document.querySelector('#tracking');
+trackingBtn.addEventListener('click', function () {
+  const id = '0123456789';
+  if (trackingID.value === id) {
+    alert('Your parcel is on the way .....');
+  } else {
+    alert('Please enter your 10 digit tracking id');
+  }
+});
+
+// ====================
+// email validation
+// ====================
+
+const pattern = /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+
+function validMail(email) {
+  if (!email) return false;
+
+  const atPattern = email.split('@');
+
+  if (atPattern.length !== 2) return false;
+
+  const account = atPattern[0];
+  const address = atPattern[1];
+
+  if (account.length > 64) return false;
+  else if (address.length > 255) return false;
+
+  const dotPattern = address.split('.');
+  if (
+    dotPattern.some(function (part) {
+      return part.length > 63;
+    })
+  )
+    return false;
+
+  if (!pattern.test(email)) return false;
+
+  return true;
+}
+
+const input = document.querySelector('.subscribe__email');
+const submitBtn = document.querySelector('.subscribe__btnb');
+submitBtn.addEventListener('click', function () {
+  const mail = input.value;
+  const isValid = validMail(mail);
+
+  if (isValid) {
+    alert('Thank you for subscribe');
+  } else if (mail == '') {
+    alert('Please enter your email id');
+  } else {
+    alert('Please enter your correct email id');
+  }
+});
